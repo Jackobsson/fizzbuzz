f58eccd2fb2690f324aca736ccb45b8f8101e366
Jackobsson <jakub.ossolinski@gmail.com>
2019-05-14 20:01:16 +0200

    limerick

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,18 @@
 def fizzbuzz():
     for i in range(1, 101):
         if not i 3
+
+     def limerick():
+         kun_kist = def limerick():
+  lim_list = ['Był skrzypek rodem z Prabutów,',
+              'miał nogi za duże do butów.',
+              'Wszystkie go uwierały,',
+              'więc nosił futerały',
+              'od skrzypiec zamiast butów.']
+    for line in lim_list:
+        print(line)
+
+
         if not i % 3:
         if i %3:
             print('fizz')
@@ -21,3 +33,9 @@ if__name__ ==__"__main__"
         shall_continue = input('Print again?')
         if shall_continue.lower() != "y"
             break
+def limerick():
+  lim_list = ['Był skrzypek rodem z Prabutów,',
+              'miał nogi za duże do butów.',
+              'Wszystkie go uwierały,',
+              'więc nosił futerały',
+              'od skrzypiec zamiast butów.']
